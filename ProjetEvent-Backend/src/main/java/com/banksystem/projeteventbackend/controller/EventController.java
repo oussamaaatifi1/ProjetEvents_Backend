36ca5a98778ae37f8a0f7e5c43897dcdf1f148a9
oussamaaatifi1 <nisowpro@gmail.com>
2024-07-26 17:27:52 +0100

    update partie security

--- ProjetEvent-Backend/src/main/java/com/banksystem/projeteventbackend/controller/EventController.java
+++ ProjetEvent-Backend/src/main/java/com/banksystem/projeteventbackend/controller/EventController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/auth/Admin/events")
+@RequestMapping("/api/auth/Admin/events")
 public class EventController {
 
     private final EventService eventService;
