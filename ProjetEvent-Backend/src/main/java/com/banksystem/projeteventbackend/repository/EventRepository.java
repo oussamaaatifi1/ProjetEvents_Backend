c30b5ef897d4da05d4312195a8c8755786c7017a
Reda Ouadi <redaouadi34@gmail.com>
2024-07-27 14:12:58 +0100

    Dashbord et afficher les statistique

--- ProjetEvent-Backend/src/main/java/com/banksystem/projeteventbackend/repository/EventRepository.java
+++ ProjetEvent-Backend/src/main/java/com/banksystem/projeteventbackend/repository/EventRepository.java
@@ -2,8 +2,13 @@ package com.banksystem.projeteventbackend.repository;
 
 import com.banksystem.projeteventbackend.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Query("SELECT COUNT  (*) from  Event ")
+    long countEvents();
+
 }
